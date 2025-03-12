e0ea70b48d9eb20baf0676a88eea580bc529d005
RebelWind <vataiorg@gmail.com>
2025-03-12 14:45:34 +0300

    updated

--- Dockerfile
+++ Dockerfile
@@ -41,7 +41,7 @@ RUN apk add tiff --no-cache --repository http://dl-3.alpinelinux.org/alpine/edge
 RUN apk add libgdiplus --no-cache --repository http://dl-3.alpinelinux.org/alpine/edge/community/ --allow-untrusted
 RUN apk add libc-dev tzdata curl bash --no-cache
 
-ENV OTEL_RESOURCE_ATTRIBUTES=deployment.environment=staging,service.version=1.0.0
+ENV OTEL_RESOURCE_ATTRIBUTES=service.version=1.0.0
 ENV OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318/v1/traces
 ENV OTEL_SERVICE_NAME=sample-app
 ENV OTEL_TRACES_EXPORTER=otlp
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Alpine Linux için dinamik bağlayıcı sembolik bağlantısı oluştur
 ln -s /lib/libc.musl-x86_64.so.1 /lib/ld-linux-x86-64.so.2
 
-# OpenTelemetry instrument.sh betiğini çalıştır
 if [ -f "/root/.otel-dotnet-auto/instrument.sh" ]; then
     echo "OpenTelemetry instrument.sh betiğini çalıştırıyorum..."
     source /root/.otel-dotnet-auto/instrument.sh
@@ -13,5 +11,4 @@ if [ -f "/root/.otel-dotnet-auto/instrument.sh" ]; then
     env | grep DOTNET
 fi
 
-# Uygulamayı başlat
 exec dotnet Nop.Web.dll
